0fa30c31176b69bbe18cdc4c04285191162f4d11
pyoganathan99 <pyoganathan99@gmail.com>
2017-07-15 10:52:33 +0000

    Add files via upload

---
+++ sem.js
@@ -0,0 +1,5 @@
+var sem_text = document.getElementById('sem_text');
+sem_text.textContent = document.getElementById('sem_slider').value;
+function changeSem(value) {
+sem_text.textContent = value;
+}
